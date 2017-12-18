6458be2d88d509fc1e8ed359a8e7c4dda68aa026
capan <capanh@gmail.com>
2017-12-18 16:10:56 +0300

    transition

--- basemap.js
+++ basemap.js
@@ -4,14 +4,14 @@ var il;
 var points;
 
 var geoMercator = d3.geoMercator().scale(5500)
-  .center( [28.739557, 39.871648] )
-  .translate([300,300]);
-  
+  .center([28.739557, 39.871648])
+  .translate([300, 300]);
+
 
 var map_svg = d3.select("body")
-.append("svg")
-.attr("width",viewWidth)
-.attr("height",viewHeight);
+  .append("svg")
+  .attr("width", viewWidth)
+  .attr("height", viewHeight);
 
 var g = map_svg.append("g");
 
@@ -20,79 +20,79 @@ var geoPath = d3.geoPath().projection(geoMercator);
 //regions
 var g = map_svg.append("g");
 g.selectAll("path")
-   .data(tr_geojson.features)
-   .enter()
-   .append("path")
-   .attr("d",geoPath)
-   .attr("stroke-width", "1")
-   .attr("stroke", "white")
-   .on("mouseover",handleMouseOver)
-   .on("mouseout",handleMouseOut)
-   .on("click",handleClick);
+  .data(tr_geojson.features)
+  .enter()
+  .append("path")
+  .attr("d", geoPath)
+  .attr("stroke-width", "1")
+  .attr("stroke", "white")
+  .on("mouseover", handleMouseOver)
+  .on("mouseout", handleMouseOut)
+  .on("click", handleClick);
 
-   //airports
-  var g1 = map_svg.append("g");
-    g1.selectAll("path")
-   .data(airports_json.features)
-   .enter()
-   .append("path")
-   .attr("fill","#ff0000")
-   .attr("d",geoPath)
-   .on("click", function() {
-          var coords = d3.mouse(this);
-          console.log(coords);
-          
-      });
+//airports
+var g1 = map_svg.append("g");
+g1.selectAll("path")
+  .data(airports_json.features)
+  .enter()
+  .append("path")
+  .attr("fill", "#ff0000")
+  .attr("d", geoPath)
+  .on("click", function () {
+    var coords = d3.mouse(this);
+    console.log(coords);
 
-      //interaction
+  });
 
-    function handleMouseOver(d, i) { 
-      
-      if(d.properties.name == "Tekirdağ"){
-        d3.select(this).transition().
-        style(
-           "fill", "red"
-        );
-        myTransition();
+//interaction
+function handleMouseOver(d, i) {
 
-      } else if(d.properties.name == "Adana"){
-        
-        d3.select(this).transition().
-        style(
-           "fill", "red"
-        );
-        myTransition();
-      }
-      else {
-        var coords = d3.mouse(this);
-        d3.select(this).transition().
-        style(
-           "fill", "orange"
-        );
-      }
+  if (d.properties.name == "Tekirdağ") {
+    d3.select(this).transition().
+      style(
+      "fill", "red"
+      );
 
-            d3.select(this)
-            .append("svg:title")
-            .text(function(d){return d.properties.name})
-                    .style("text-anchor", "middle")
-                    .style("fill", "#555")
-                    .style("font-family", "Arial")
-                    .style("font-size", 50);
 
-                    il = d.properties.name;
-              
-            }
 
-          function handleMouseOut(d,i){
-            d3.select(this).attr("fill","black");
-           
-          }
+  } else if (d.properties.name == "Adana") {
 
-          function handleClick(d,i){
-            var coords = d3.mouse(this);
-            document.getElementById("il").innerHTML = d.properties.name;
-            myTransition();
-            circle.style("opacity", 1).style("stroke", "gray")
-            .style("fill","red");
+    d3.select(this).transition().
+      style(
+      "fill", "red"
+      );
+    il = d.properties.name;
+    if (il = "Adana") {
+      points = [[941, 651], [477, 311], [221, 138]];
+    } else if (il = "Tekirdağ") {
+      points = [[221, 138], [477, 311], [941, 651]];
+    };
+    myTransition(points);
+  }
+  else {
+    var coords = d3.mouse(this);
+    d3.select(this).transition().
+      style(
+      "fill", "orange"
+      );
+  }
 
-          }
+  d3.select(this)
+    .append("svg:title")
+    .text(function (d) { return d.properties.name })
+    .style("text-anchor", "middle")
+    .style("fill", "#555")
+    .style("font-family", "Arial")
+    .style("font-size", 50);
+}
+
+function handleMouseOut(d, i) {
+  d3.select(this).transition().style("fill", "black");
+  d3.select("#fp").transition().remove();
+
+}
+
+function handleClick(d, i) {
+  var coords = d3.mouse(this);
+  document.getElementById("il").innerHTML = d.properties.name;
+}
--- move.js
+++ move.js
@@ -1,66 +1,45 @@
-var lineGenerator = d3.line().curve(d3.curveCardinal);
 
-if (il="Adana"){
-  points = [[941,651],[477,311],[221,138]];
-}else if(il="Tekirdağ"){
-   points = [[221,138],[477,311],[941,651]];
-};
-
-//creating flight path data with line generator
-var pathData = lineGenerator(points);
-//appending new group g2 with id fPath 
-var g2 = map_svg.append("g").attr("id","fPath");
-//appending created flight path to variable 
-var path = g2.append("path").data([points]).attr("d", d3.line());
-
-//flight path
-g2.selectAll('path')
-  .data(points)
-  .attr('d', pathData)
-  .attr("stroke", "transparent")
-  .attr("fill","transparent");
-
-// Also draw points for reference
-// d3.select('svg1')
-//   .selectAll('circle')
-//   .data(points)
-//   .enter()
-//   .append('circle')
-//   .attr('cx', function(d) {
-//     return d[0];
-//   })
-//   .attr('cy', function(d) {
-//     return d[1];
-//   })
-//   .attr('r', 10);
-
+function myTransition(x1) {
   var tr_bl = true;
-  
-        var circle = map_svg.append("circle")
-              .attr("r", 10)
-              .attr("transform", "translate(" + points[0] + ")")
-              // .style("stroke", "gray")
-              // .style("fill","red")
-              .style("opacity", 0);
+  if (tr_bl) {
 
+    var lineGenerator = d3.line().curve(d3.curveCardinal);
+    
+    //creating flight path data with line generator
+    var pathData = lineGenerator(x1);
+    //appending new group g2 with id fPath 
+    var g2 = map_svg.append("g").attr("id","fp");
+    //appending created flight path to variable 
+    var path = g2.append("path").data([x1]).attr("d", d3.line());
+    
+    //flight path
+    g2.selectAll('path')
+      .data(x1)
+      .attr('d', pathData)
+      .attr("stroke", "blue")
+      .attr("fill", "transparent");       
+    //flying object
+    var circle = map_svg.append("circle")
+      .attr("r", 10)
+      .attr("transform", "translate(" + x1[0] + ")")
+      .style("opacity", 1)
+      .style("fill","red");
 
-      function myTransition() {
-      if (tr_bl){
-      circle.transition()
+    circle.transition()
       .duration(10000)
-      .attrTween("transform", translateAlong(path.node()));
-
+      .attrTween("transform", translateAlong(path.node()))
+      .each("end", remove());  
       
-      } 
-      else {
-        circle.transition();
-      }
+  }
+  else {
+    circle.transition();
+  }
 }
 
-      function translateAlong(path) {
+function translateAlong(path) {
   var l = path.getTotalLength();
-  return function(d, i, a) {
-    return function(t) {
+  return function (d, i, a) {
+    return function (t) {
       var p = path.getPointAtLength(t * l);
       return "translate(" + p.x + "," + p.y + ")";
     };
