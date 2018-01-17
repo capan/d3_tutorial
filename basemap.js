f2e7a0fdffd8c0efe4246bb61cafe1a53941f58b
capan <capanh@gmail.com>
2018-01-17 23:16:03 +0300

    mouseOver
    mouseOVer'dan devam

--- basemap.js
+++ basemap.js
@@ -2,8 +2,6 @@ var viewHeight = window.innerHeight;
 var viewWidth = window.innerWidth;
 var il;
 var points;
-var airportPoints = []; var j = 0; var newArray = [];
-var dynamicFlightRoute = []; var destinationArray = [];
 // d3.select(window).on("load", APCentroid(true));
 var geoMercator = d3.geoMercator().scale(5500)
   .center([28.739557, 39.871648])
@@ -39,36 +37,53 @@ g1.selectAll("path")
   });
 //interaction
 function handleMouseOver(d, i) {
+  var dynamicFlightRoute = [];
   var airportObjects = [];
-  console.log(dynamicFlightRoute, airportObjects, destinationArray);
-  // dynamicFlightRoute.length = 0; airportObjects.length = 0; destinationArray.length = 0;
   dynamicFlightRoute = destinationFunction(d.properties.name);
   airportObjects = APMeanCoord(dynamicFlightRoute);
-  console.log(airportObjects);
+  // console.log(airportObjects);
+  if (airportObjects.length > 0) {
+    var n = airportObjects.length;
+    var points = [];
+    d3.select(this).transition().
+      style(
+      "fill", "red"
+      );
+      for(i = 0;i<=n;i++){
+        points[i] = 
+      }
 
-  if (d.properties.name == "Tekirdağ") {
-    d3.select(this).transition().
-      style(
-      "fill", "red"
-      );
-    points = [[221, 138], [941, 651]];
-    myTransition(points);
-  } else if (d.properties.name == "Adana") {
-    d3.select(this).transition().
-      style(
-      "fill", "red"
-      );
-    il = d.properties.name;
-    points = [[941, 651], [221, 138]];
-    myTransition(points);
-  }
-  else {
-    var coords = d3.mouse(this);
+    // if (d.properties.name == "Tekirdağ") {
+    //   d3.select(this).transition().
+    //     style(
+    //     "fill", "red"
+    //     );
+    //   points = [[221, 138], [941, 651]];
+    //   myTransition(points);
+    // } else if (d.properties.name == "Adana") {
+    //   d3.select(this).transition().
+    //     style(
+    //     "fill", "red"
+    //     );
+    //   il = d.properties.name;
+    //   points = [[941, 651], [221, 138]];
+    //   myTransition(points);
+    // }
+    // else {
+    //   var coords = d3.mouse(this);
+    //   d3.select(this).transition().
+    //     style(
+    //     "fill", "orange"
+    //     );
+    // }
+
+  } else {
     d3.select(this).transition().
       style(
       "fill", "orange"
       );
   }
+
   //city label on mouse over
   d3.select(this)
     .append("svg:title")
@@ -88,10 +103,11 @@ function handleClick(d, i) {
 }
 
 function destinationFunction(name) {
+  var destinationArray = [];
   var i = 0;
   route_json.routes.forEach(function (element) {
     if (element.Origin == name) {
-      destinationArray.push(element.Destination) ;
+      destinationArray.push(element.Destination);
       i++;
     }
   }
@@ -100,15 +116,14 @@ function destinationFunction(name) {
 }
 
 function APMeanCoord(someArray) {
+  var airportPoints = [];
   airportPoints.length = 0;
   var data = filterByName(someArray);
-  // console.log(destinationPorts);
   var destinationPorts = data.filter(function (element) {
     return element !== undefined;
   });
-
+  var j = 0;
   destinationPorts.forEach(function (element) {
-
     //all coordinates assigned to an array
     var cx = [];
     var cy = [];
@@ -134,19 +149,17 @@ function APMeanCoord(someArray) {
 
 
 function filterByName(destArray) {
-  for (i = 0; i <= destArray.length-1; i++) {
+  var newArray = [];
+  for (i = 0; i <= destArray.length - 1; i++) {
     newArray[i] = airports_json.features.find(function (element) {
       if (element.properties.is_in === destArray[i]) {
-        console.log('v_v Match :', element.properties.is_in);
         return element;
       }
-      console.log(')V_v( No Match :', element.properties.is_in);
     }
     );
   }
   return newArray;
 }
-
 //finding summation of an array
 function arraySum(someArray) {
   sum = someArray.reduce((a, b) => a + b, 0);
